35c066d7e53369f81d8a6eeeffb7c1da59cabaf4
srishti1332007 <srishti1332007@gmail.com>
2026-02-14 20:11:18 +0530

    Add files via upload

---
+++ day14.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[n][n];
+    int isIdentity = 1;
+
+    // Input matrix elements
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check identity matrix condition
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                if (matrix[i][j] != 1) {
+                    isIdentity = 0;
+                    break;
+                }
+            } else {
+                if (matrix[i][j] != 0) {
+                    isIdentity = 0;
+                    break;
+                }
+            }
+        }
+        if (isIdentity == 0)
+            break;
+    }
+
+    // Output result
+    if (isIdentity)
+        printf("Identity Matrix");
+    else
+        printf("Not an Identity Matrix");
+
+    return 0;
+}
